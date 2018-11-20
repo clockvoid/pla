380f7ed3557fcee140a01b24aec6486fe6eb8f0b
clockvoid <hidenari.t1997nov@gmail.com>
2018-11-21 01:54:26 +0900

    fix: pla.kt to elegant style

--- pla.kt
+++ pla.kt
@@ -1,62 +1,69 @@
 
 import java.io.File
 
+fun makeList(file: File): List<String> =
+        file.readLines().filter(String::isNotBlank).toList().map { str ->
+            str.replace("\\s".toRegex(), "")
+        }
+
+fun fromQ(lines: List<String>): List<String> =
+        lines.fold(listOf()) { qs: List<String>, str: String ->
+            qs + str.split("&")
+                    .map(String::toInt)
+                    .foldIndexed("") { i, result : String, num: Int ->
+                        result + when (num) {
+                            0 -> "\\overline{Q_${i + 1}}"
+                            else -> "Q_${i + 1}"
+                        } + when (i + 1) {
+                            7 -> ""
+                            else -> " \\cdot "
+                        }
+                    }
+
+        }
+
+fun fromD(lines: List<String>, qs: List<String>): List<String> =
+        lines.foldIndexed(listOf("", "", "", "", "", "", "")) { k, result: List<String>, str: String ->
+            str.split("&")
+                    .map(String::toInt)
+                    .foldIndexed(listOf()) { i, list: List<String>, num: Int ->
+                        list + when (num) {
+                            1 -> {
+                                val q: String  = when (result[i].split("\n").size) {
+                                    1 -> "&=& " + qs[k] + " "
+                                    else -> when (result[i].split("\n").size % 2) {
+                                        1 -> "&+& " + qs[k]
+                                        else -> "+ " + qs[k] + when (result[i].split("\n").size) {
+                                            2 -> " \\\\"
+                                            else -> " \\nonumber \\\\"
+                                        }
+                                    }
+                                }
+                                q + "\n"
+                            }
+                            else -> ""
+                        }
+                    }
+                    .zip(result) { child, parent ->
+                        parent + child
+                    }
+        }
+
 fun main(args: Array<String>) {
-    val file1 = File("plafile").absoluteFile
-    val file2 = File("plafile2").absoluteFile
-
-    val lines = file1.readLines().filter(String::isNotBlank).toList()
-    val lines2 = file2.readLines().filter(String::isNotBlank).toList()
-
-    val qs: ArrayList<String> = ArrayList()
-    val ds: ArrayList<String> = arrayListOf("", "", "", "", "", "", "")
-
-    for (str: String in lines) {
-        var i: Int = 1
-        var result = ""
-        for (num: Int in str.split("&").map{x -> x.replace(Regex("\\s"), "").toInt()}) {
-            result += when (num) {
-                0 -> "\\overline{Q_$i}"
-                else -> "Q_$i"
-            }
-            result += when (i) {
-                7 -> ""
-                else -> " \\cdot "
-            }
-            i++
-        }
-        qs.add(result)
-    }
-
-    var k: Int = 0
-    var j: ArrayList<Int> = arrayListOf(-1, -1, -1, -1, -1, -1, -1)
-    for (str: String in lines2) {
-        var i: Int = 0
-        for (num: Int in str.split("&").map{x -> x.replace(Regex("\\s"), "").toInt()}) {
-            ds[i] += when (num) {
-                1 -> {
-                    var q: String  = when (j[i]) {
-                        1 -> "&+& " + qs[k] + " "
-                        -1 -> "&=& " + qs[k] + " "
-                        0 ->  "+ " + qs[k] + " \\\\ "
-                        else -> "" + qs[k]
+    fromD(
+            makeList(File("plafileD").absoluteFile)
+            , fromQ(makeList(File("plafileQ").absoluteFile))
+    )
+            .forEachIndexed { i, str: String ->
+                println("D_${i + 1} ${
+                    when (i + 1) {
+                        7 -> "$str \\nonumber"
+                        else ->
+                            when (str.split("\n").size % 2) {
+                                1 -> "$str \\nonumber \\\\ "
+                                else -> "$str \\nonumber \\\\ \\nonumber \\\\ "
+                            }
                     }
-                    j[i] = when (j[i]) {
-                        -1 -> 0
-                        else -> 1 - j[i]
-                    }
-                    q + "\n"
-                }
-                else -> ""
+                }")
             }
-            i++
-        }
-        k++
-    }
-
-    var i: Int = 1
-    for (str: String in ds) {
-        print("D_$i " + str)
-        i++
-    }
 }
